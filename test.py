577f2c55405e5623ec203ebf584c318a01f85fef
DekaTags <dekatags@gmail.com>
2023-10-14 11:41:24 +0900

    백준 준비

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print('hello world')
+x = range(1, 100)
+
+for nbr in x:
+    print(nbr)
+
+for nbr in range(1, 50):
+    print(nbr)
+    
+i = 0
+while i < 5:
+    print(x[i])
+    i -= 1
+
+me = {'name': x, 'z': z}
+print(me['name'] and me['z'])
+
+x = [1, 2, 3, 4, 5]
+function(x)
+x^2 + 4 * x + 4
+
+print(function(x))
+
